42db1c9c9b84c039c37dd0007a88904985a75741
Junjie Chen <junjie.chen18@gmail.com>
2020-07-12 19:57:47 -0400

    feat(pointSchema): specify values of type property

--- schemas/point.js
+++ schemas/point.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const pointSchema = new Schema({ type: String });
+const pointSchema = new Schema({
+  type: {
+    type: String,
+    enum: ['Point']
+  }
+});
