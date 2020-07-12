f804c21bfac4dfb31a2c7ca6b225ef319fbd387e
Junjie Chen <junjie.chen18@gmail.com>
2020-07-12 19:56:33 -0400

    feat(connection): connect to MongoDB upon running application

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const routes = require('./routes/routes');
+
+if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') {
+  mongoose.connect('mongodb://localhost/uber_server_clone', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  });
+}
 
 const app = express();
 
